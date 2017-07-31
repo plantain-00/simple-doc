363b071a102b8c23f27ca8492c3de7c154189d40
york yao <yaoao12306@outlook.com>
2017-07-31 20:22:41 +0800

    fix script

--- clean-release.config.js
+++ clean-release.config.js
@@ -2,11 +2,12 @@ module.exports = {
   include: [
     'vendor.bundle-*.js',
     'vendor.bundle-*.css',
+    'service-worker.bundle.js',
     'index.html',
     'LICENSE',
     'package.json'
   ],
   exclude: [
   ],
-  releaseRepository: 'https://github.com/plantain-00/baogame-release.git'
+  releaseRepository: 'https://github.com/plantain-00/simple-doc-release.git'
 }

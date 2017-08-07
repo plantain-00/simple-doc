0a96c297306a1370f11ad8e0dfd9dc459efa94bc
york yao <yaoao12306@outlook.com>
2017-08-07 23:15:30 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,29 +1,36 @@
 module.exports = {
   build: [
-    'rimraf **/*.bundle-*.js *.bundle-*.css',
-    'file2variable-cli *.template.html -o variables.ts --html-minify',
-    'tsc',
-    'lessc index.less > index.css',
-    'cleancss -o vendor.bundle.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tree-component/tree.min.css ./node_modules/highlight.js/styles/routeros.css',
-    'cleancss -o index.bundle.css index.css',
-    'webpack --display-modules --config webpack.config.js',
+    {
+      js: [
+        'file2variable-cli *.template.html -o variables.ts --html-minify',
+        'tsc',
+        'webpack --display-modules --config webpack.config.js'
+      ],
+      css: {
+        vendor: 'cleancss -o vendor.bundle.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tree-component/tree.min.css ./node_modules/highlight.js/styles/routeros.css',
+        index: [
+          'lessc index.less > index.css',
+          'cleancss -o index.bundle.css index.css'
+        ]
+      },
+      clean: 'rimraf **/*.bundle-*.js *.bundle-*.css'
+    },
     'rev-static --config rev-static.config.js',
     'sw-precache --config sw-precache.config.js --verbose',
     'uglifyjs service-worker.js -o service-worker.bundle.js'
   ],
-  lint: [
-    `tslint "*.ts"`,
-    `standard "**/*.config.js"`,
-    `stylelint "**/*.less"`
-  ],
+  lint: {
+    ts: `tslint "*.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "**/*.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  fix: {
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "**/*.less"`
+  },
+  release: `clean-release`
 }
